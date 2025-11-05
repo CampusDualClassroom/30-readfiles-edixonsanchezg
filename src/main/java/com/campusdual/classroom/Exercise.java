1122ab8231f7009108af7ba1fbe3adb58e35ea36
Edixon Sánchez <ediiixonsanchezg@gmail.com>
2025-11-05 17:26:51 +0100

    ejercicio 30 completado

--- src/main/java/com/campusdual/classroom/Exercise.java
+++ src/main/java/com/campusdual/classroom/Exercise.java
@@ -1,7 +1,23 @@
 package com.campusdual.classroom;
 
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
 public class Exercise {
     public static void main(String[] args) {
+        try (InputStream inputStream = Exercise.class.getResourceAsStream("/lorem.txt");
+             BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream))) {
+
+            String line;
+            while ((line = reader.readLine()) != null) {
+                System.out.println(line);
+            }
+
+        } catch (Exception e) {
+            System.out.println("Ocurrió un error leyendo el archivo.");
+        }
+
 
     }
 }
